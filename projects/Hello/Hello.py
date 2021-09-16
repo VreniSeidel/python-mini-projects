00b77718aaa0223aa973c341e537c8b14884ae62
zoe <zoe@pop-os.localdomain>
2021-09-16 23:53:51 +0500

    Hello world and venv python

--- projects/Hello/Hello.py
+++ projects/Hello/Hello.py
@@ -1,3 +1,14 @@
 # This is simple program to show how print statement works
+from tkinter import *
 
-print('Hello Python World')
+
+X = 400 # X coordinate
+Y = 400 # Y coordinate
+
+root=Tk()                   # Window it self
+root.geometry(f"{X}x{Y}")   # Window size
+root.title("Python")        # Title
+
+Label(root, text="Hello python world!").place(x=X-270, y=Y-230) # Text from window
+
+root.mainloop() # End of the main loop
